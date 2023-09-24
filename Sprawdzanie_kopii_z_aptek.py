dca7550bf5d1430e2253de0a9970224bb6e542e0
practel <145769975+practel@users.noreply.github.com>
2023-09-24 20:51:28 +0200

    Poprawiony działa
    Przebudowana metoda def sprawdz_plik()
    działa ok

--- Sprawdzanie_kopii_z_aptek.py
+++ Sprawdzanie_kopii_z_aptek.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import smtplib
 import fnmatch
+import glob
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 class Apteka:
@@ -45,9 +46,10 @@ class KatalogAptek:
         wynikbrak = []
         wiek_pliku = 0
         rozmiar1 = 0.01
-        procent_wielkosci_pliku = 0
+        plik_poprzedni = ""
         max_wielposc_pliku = max_wielkosc_1
         rodzaj_bledu ='Brak pliku'
+        lista_plikow_apteki = [] # lista plików do analizy
         for plik in os.listdir(sciezka_katalogu):
             if fnmatch.fnmatch(plik, szukany_plik):
                 pelna_sciezka_pliku = os.path.join(sciezka_katalogu, plik)
@@ -55,26 +57,32 @@ class KatalogAptek:
                     data_modyfikacji = datetime.date.fromtimestamp(os.path.getmtime(pelna_sciezka_pliku))
                     rozmiar_pliku = os.path.getsize(pelna_sciezka_pliku)
                     wiek = (dzisiejsza_data - data_modyfikacji).days
-                    if wiek == wiek_pliku + 1 and rozmiar_pliku > 500 and plik == szukany_plik: # pobieram wielkość pliku starszego o 1 dzień
-                        rozmiar1 = rozmiar_pliku
-                    if wiek == wiek_pliku and rozmiar_pliku > 500: # Jeżeli są pliki dzisiejsze > 500
-                        procent_wielkosci_pliku = round(((rozmiar_pliku - rozmiar1) / rozmiar1) * 100, 2)
-                        if rozmiar1 == 0.01:
-                            procent_wielkosci_pliku = 0.01
-                        if int(rozmiar_pliku) < int(max_wielposc_pliku):
-                            if -2.9 <= procent_wielkosci_pliku <= 2.9:
-                                wynikjest.append(
-                                    f"OK: {nazwa_apteki} {data_modyfikacji} Rozmiar: {round(rozmiar_pliku/ 1024**3, 2)} GB   proc {procent_wielkosci_pliku}")
-                            else:
-                                rodzaj_bledu = 'błąd wielkości '
-                        else:
-                            rodzaj_bledu = 'Plik za duży '
-                    else:
-                        wynikbrak.append(f"{nazwa_apteki} Rozmiar: {rozmiar_pliku} Jest {data_modyfikacji} ")
-        if wynikjest:  # Jeśli wynik zawiera elementy, to zwracamy listę z wynikami
+                    #print(f'Wynik testu {wiek} {nazwa_apteki} {plik} {rozmiar_pliku}')
+                    wynik = nazwa_apteki, wiek, plik, rozmiar_pliku
+                    #print(wynik)
+                    lista_plikow_apteki.append(wynik) #, szukany_plik, wiek_pliku max_wielposc_pliku)
+        posortowana_lista = sorted(lista_plikow_apteki, key=lambda x: x[1])
+        #print(posortowana_lista)
+
+        najmlodszy = (posortowana_lista[0]) # pierwszy element listy posortowanej
+        wielkosc_najlodszego = najmlodszy[3]
+        najmlodszy_1 = (posortowana_lista[1]) # kolejny z najmlodszych element listy posortowanej
+        wielkosc_najmlodszego_1 = najmlodszy_1[3]
+        wiek_najmlodszego = najmlodszy[1]
+        procent_wielkosci_pliku = round(((wielkosc_najlodszego - wielkosc_najmlodszego_1) / wielkosc_najmlodszego_1) * 100, 2)
+
+        if wiek_najmlodszego >0:
+            wynikbrak = (f'Brak kopii {najmlodszy[0]} plik: {najmlodszy[2]} ma {wiek_najmlodszego} dni ')
+            return wynikbrak
+        if procent_wielkosci_pliku >= 7 or procent_wielkosci_pliku <= -7 :
+            wynikbrak = (
+                f'Brak Blad wieklosci pliku {najmlodszy[0]}'
+                f' plik: {najmlodszy[2]} ma {wielkosc_najlodszego}'
+                f' poprzedni mial {wielkosc_najmlodszego_1} {procent_wielkosci_pliku}')
+            return wynikbrak
+        if procent_wielkosci_pliku < 10 and wiek_najmlodszego == 0 and wielkosc_najlodszego < int(max_wielposc_pliku):
+            wynikjest = (f'OK {najmlodszy[0]}, plik: {najmlodszy[2]}, wielkosc: {round(najmlodszy[3]/ 1024**3, 2)} GB, procent: {procent_wielkosci_pliku}% ')
             return wynikjest
-        else:
-            return [f'Blad: {rodzaj_bledu} {wynikbrak[0]}  {szukany_plik}']
 
     def wczytaj_liste_apteki(self):
         lista_aptek = []
@@ -84,51 +92,37 @@ class KatalogAptek:
                 apteka.plik_2dzienna, apteka.sciezka_1godzinowa, apteka.plik_1godzinowa, apteka.sciezka_2godzinowa, apteka.plik_2godzinowa, apteka.max_wielkosc_1)
             lista_aptek.append(pojedyncza_apteka)
         return lista_aptek
-
-    def sprawdz_pliki(self):
-        katalog.wczytaj_apteki_z_pliku()
+    def test_kopii_dziennej(self, jaki):
         lista_aptek = katalog.wczytaj_liste_apteki()  # Tworzy listę aptek - lista_aptek
         for apteki in lista_aptek:
+            #print(apteki)
             if apteki[2] != '':  # jeżeli jest wpisana ścieżka w trzecim polu to sprawdzaj plik
-                wynik1 = katalog.sprawdz_plik(sciezka_katalogu=apteki[2], szukany_plik=apteki[3], nazwa_apteki=apteki[1],
-                                              max_wielkosc_1=apteki[10])
-                # print(wynik1)
-            if apteki[4] != '':  # jeżeli jest wpisana ścieżka w piątym polu to sprawdzaj plik
-                wynik2 = katalog.sprawdz_plik(sciezka_katalogu=apteki[4], szukany_plik=apteki[5], nazwa_apteki=apteki[1],
-                                              max_wielkosc_1=apteki[10])
-                # print(wynik2)
-            if apteki[6] != '':  # jeżeli jest wpisana ścieżka w piątym polu to sprawdzaj plik
-                wynik3 = katalog.sprawdz_plik(sciezka_katalogu=apteki[6], szukany_plik=apteki[7], nazwa_apteki=apteki[1],
-                                              max_wielkosc_1=apteki[10])
-            if apteki[8] != '':  # jeżeli jest wpisana ścieżka w piątym polu to sprawdzaj plik
-                wynik4 = katalog.sprawdz_plik(sciezka_katalogu=apteki[8], szukany_plik=apteki[9], nazwa_apteki=apteki[1],
-                                              max_wielkosc_1=apteki[10])
+                wynik1 = katalog.sprawdz_plik(
+                    sciezka_katalogu=apteki[2], szukany_plik=apteki[3],
+                    nazwa_apteki=apteki[1],max_wielkosc_1=apteki[10])
 
-            for element in wynik1:
-                if element.startswith("Blad"):
-                    print(element)
-                    pass
-                elif element.startswith('OK'):
-                    print(element)
-                    pass
-            for element in wynik2:
-                if element.startswith("Blad"):
-                    # print(element)
-                    pass
-                elif element.startswith('OK'):
-                    # print(element)
-                    pass
-            # for element in wynik3:
-            #    if element.startswith("Blad"):
-            #        #print(element)
-            #        pass
-            # for element in wynik4:
-            #    if element.startswith("Blad"):
-            #        #print(element)
-            #        pass
+            if wynik1.startswith(jaki):
+                print(wynik1)
+                pass
+
+    def start(self):
+        katalog.wczytaj_apteki_z_pliku()
+        #lista_aptek = katalog.wczytaj_liste_apteki()  # Tworzy listę aptek - lista_aptek
+        while True:
+            print('1 - Pokaż apteki bez kopii dziennej')
+            print('2 - Pokaż apteki z poprawną kopią')
+            wybor = input(f'Podaj opcje: ')
+            if wybor == "0":
+                break
+            elif wybor == "1":
+                jaki = "Brak"
+                katalog.test_kopii_dziennej(jaki)
+            elif wybor == '2':
+                jaki = "OK"
+                katalog.test_kopii_dziennej(jaki)
 
 # Program do weryfikacji wysyłania kopii bazy
 if __name__ == "__main__":
     katalog = KatalogAptek()
-    katalog.sprawdz_pliki()
+    katalog.start()
 
